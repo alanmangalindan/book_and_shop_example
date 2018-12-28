6b041e66117dd4731472857f7992f23086aff994
Alan Mangalindan <aman424@aucklanduni.ac.nz>
2018-12-29 00:02:02 +1300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Set up express
+var express = require('express');
+
+var app = express();
+
+// Listen on port 3000
+app.set('port', process.env.PORT || 3000);
+
+// Set up handlebars
+var handlebars = require('express-handlebars');
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
