6955bc90a1d32d155a1004424efd9d85e4c783a4
Alan Mangalindan <aman424@aucklanduni.ac.nz>
2018-12-29 09:46:38 +1300

    home page draft

--- app.js
+++ app.js
@@ -28,15 +28,11 @@ app.use(session({
 }));
 
 // Set up password hashing
-var scrypt = require("scrypt");
-var scryptParameters = scrypt.paramsSync(0.1);
-var key = new Buffer("myPassword"); //could also be a string
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 
-var kdfResult = scrypt.kdfSync(key, scryptParameters);
-
-//Synchronous
-scrypt.verifyKdfSync(kdfResult, key); // returns true
-scrypt.verifyKdfSync(kdfResult, "incorrect password"); // returns false
 
 // require formidable for handling audio/video uploads
 var formidable = require("formidable");
@@ -45,16 +41,16 @@ var formidable = require("formidable");
 var dao = require('./dao.js');
 
 // Custom module for authentication stuff
-var auth = require('./authentication.js');
-auth.initialize(app);
-auth.setupLogin("/login", "/orderHistory", "/login?loginFail=true");
-auth.setupLogout("/logout", "/animals?loggedOut=true");
+// var auth = require('./authentication.js');
+// auth.initialize(app);
+// auth.setupLogin("/login", "/orderHistory", "/login?loginFail=true");
+// auth.setupLogout("/logout", "/animals?loggedOut=true");
 
 //--------------------- ROUTE HANDLERS -------------------------------------------
 
 app.get('/', function (req, res) {
 
-    res.render('home', data);
+    res.render('home');
 });
 
 
