6037b026503b104b280046f457f45af97cc75bfe
Alan Mangalindan <aman424@aucklanduni.ac.nz>
2019-01-01 09:01:24 +1300

    added sql file to create DB tables

---
+++ book_and_shop_db-init.sql
@@ -0,0 +1,31 @@
+--create database tables
+
+create table Users (
+    username varchar(50) not null primary key,
+    password varchar(50) not null,
+    fname varchar(50) not null,
+    lname varchar(50) not null,
+    activeFlag integer not null,
+    check (activeFlag = 0 or activeFlag = 1)
+);
+
+create table Professionals (
+    profId integer not null primary key autoincrement,
+    fname varchar(50) not null,
+    lname varchar(50) not null,
+    location varchar(50),
+    specialty varchar(50)
+);
+
+create table Bookings (
+    bookedBy varchar(50) not null,
+    profId integer not null,
+    bookingDate date,
+    bookingTime time,
+    location varchar(50),
+    notes text,
+    primary key (profId, bookingDate, bookingTime),
+    foreign key (bookedBy) references Users (username),
+    foreign key (profId) references Articles (profId)
+);
+
