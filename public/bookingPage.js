2b41bb7fd2d150824720511f43198c850da82bb7
Alan Mangalindan <aman424@aucklanduni.ac.nz>
2019-01-03 10:22:36 +1300

    working table for selected professional

--- public/bookingPage.js
+++ public/bookingPage.js
@@ -14,21 +14,24 @@ function getProfDetails(professionalId) {
 function getProfBookings(professionalId) {
     $.get("/getProfBookings/" + professionalId, function (data, status) {
         var profBookings = JSON.parse(data);
+        console.log(profBookings);
         if (profBookings) {
             var bookingsDisplay = $('#profBookings');
             bookingsDisplay.empty();
             var profBookingsTable = '<table class="table table-striped table-responsive"> ' + 
-            '<thead> <tr> <th>Name</th> <th>Date</th> <th>Time</th> <th>Location</th> </tr> ' + 
+            '<thead> <tr> <th>Name</th> <th>Date</th> <th>Time</th> <th>Location</th> <th></th> </tr> ' + 
             '</thead> <tbody>';
             for (var i = 0; i < profBookings.length; i++) {
                 profBookingsTable += '<tr> ' +
-                '<td><a href="/editBooking/' + profBookings[i].bookingId + '">' + profBookings[i].fname + ' ' + profBookings[i].lname + ' </a></td> ' +
+                '<td>' + profBookings[i].fname + ' ' + profBookings[i].lname + '</td> ' +
                 '<td>' + profBookings[i].bookingDate + '</td>' +
                 '<td>' + profBookings[i].bookingTime + '</td>' +
                 '<td>' + profBookings[i].location + '</td>' +
+                '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#booking-"' + profBookings[i].bookingId + '>View</button></td>' +
                 '</tr>'; 
             }
             profBookingsTable += '</tbody> </table>';
+            console.log(profBookingsTable);
             bookingsDisplay.append(profBookingsTable);
         }
     });
